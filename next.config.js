fdccb60902b9a8a80cba53a679a4a2fce2ad7a37
Deftu <deftudev@gmail.com>
2022-01-29 18:32:25 +0200

    new: Testing GitHub redirect.

--- next.config.js
+++ next.config.js
@@ -6,6 +6,11 @@ module.exports = {
                 source: "/discord",
                 destination: "https://discord.gg/k7Gj5DYSyg",
                 permanent: true
+            },
+            {
+                source: "/github/*",
+                destination: "https://github.com/IncrieTechnologies/*",
+                permanent: true
             }
         ]
     }
